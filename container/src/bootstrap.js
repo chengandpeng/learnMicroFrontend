e45f53a66f80d992b1d516e4d1e9578e8c4c6196
Cheng Peng <chengandpeng@gmail.com>
2021-07-27 00:03:03 +0800

    section 3

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as pMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
 console.log('hahha');
+
+pMount(document.querySelector('#dev-1'))
+cartMount(document.querySelector('#dev-2'))
